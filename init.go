2eae9ab4fc25416668fb0d3d64ea250c9e1d7ea8
白云辉 <baiyunhui@yuanben.org>
2018-05-22 16:37:02 +0800

    调整注册结构

--- init.go
+++ init.go
@@ -25,20 +25,3 @@ func SetCfg(cfg1 *KConfig) {
 		logger.SetHandler(log.LvlFilterHandler(log.LvlError, log.StreamHandler(os.Stderr, log.LogfmtFormat())))
 	}
 }
-
-func init() {
-	hm := GetHManager()
-
-	MustNotErr(hm.Registry(PingReqT, &PingReq{}))
-
-	MustNotErr(hm.Registry(FindNodeReqT, &FindNodeReq{}))
-	MustNotErr(hm.Registry(FindNodeRespT, &FindNodeResp{}))
-
-	MustNotErr(hm.Registry(KVSetReqT, &KVSetReq{}))
-	MustNotErr(hm.Registry(KVGetReqT, &KVGetReq{}))
-	MustNotErr(hm.Registry(KVGetRespT, &KVGetResp{}))
-
-	MustNotErr(hm.Registry(GKVSetReqT, &GKVSetReq{}))
-	MustNotErr(hm.Registry(GKVGetReqT, &GKVGetReq{}))
-	MustNotErr(hm.Registry(GKVGetRespT, &GKVGetResp{}))
-}
